0adff2980f34061c1b9b77ac261a694498c6bfc2
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-08-03 13:34:41 -0400

    notes

---
+++ inclassnotes.py
@@ -0,0 +1,38 @@
+hash_data = [None] * 8
+
+def hash_function(s):
+    #take input.. return bytes size
+    bytes_list = s.encode()
+    
+    total = 0
+    
+    for b in bytes_list:
+        total += b
+    
+    return total
+
+def get_index(s):
+    hash_value = hash_function(s)
+    
+    return hash_value % 8
+
+def put(k, v):
+    # for a given key, store a value in the hash table
+    index = get_index(k)
+    hash_data[index] = v
+
+def get(k):
+    # return the value from given key
+    index = get_index(k)
+    return hash_data[index]
+    
+# inserting
+put("bobby", "hello, world")
+put("jerk", 90210)
+
+
+# print all hashdata
+print(hash_data)
+# get value by key
+print(get("bobby"))
+print(get("jerk"))
